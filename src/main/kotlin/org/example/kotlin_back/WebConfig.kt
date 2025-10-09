33f906913b64ba70f4f40cf31e34ee1b2eb31637
cedricsanchez <mycedmail@gmail.com>
2025-10-09 16:31:27 +0200

    Update CORS configuration to restrict paths and add new allowed origins for production environments

--- src/main/kotlin/org/example/kotlin_back/WebConfig.kt
+++ src/main/kotlin/org/example/kotlin_back/WebConfig.kt
@@ -2,16 +2,19 @@ package org.example.kotlin_back
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+
 @Configuration
-@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+        registry.addMapping("/api/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://echap-o-pital.topconfig-pc.fr",
+                "http://echap-o-pital.topconfig-pc.fr"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
